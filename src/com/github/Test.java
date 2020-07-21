10b1acfd91998731d1b8c4085177c337b314b2c7
zhaohang <978609196@qq.com>
2020-07-21 17:48:53 +0800

    学习git第一次提交

---
+++ src/com/github/Test.java
@@ -0,0 +1,14 @@
+package com.github;
+
+/**
+ * @program: apple
+ * @author: 赵航
+ * @create: 2020-07-21 17:45
+ */
+
+public class Test
+{
+    public static void main(String[] args) {
+        //第一次提交
+    }
+}
